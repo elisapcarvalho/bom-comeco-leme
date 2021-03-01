084c2ad6974474aea94f3f1484a327649d866714
Leonardo Victor Soares Medeiros <leovictor@hotmail.com>
2021-03-01 15:15:51 -0300

    Made some improvements on hangman project

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -2,19 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
   const MAX_ATTEMPTS = 9;
   const hangman = document.getElementById("hangmanPicture");
   const restartButton = document.getElementById("restart");
-  const restartContainer = document.getElementById("restart-container");
+  // const restartContainer = document.getElementById("restart-container");
+  // const resultText = document.getElementById("result");
+  const imageResult = document.getElementById("image-result");
   const alphabetContainer = document.getElementById("alphabet-container");
-  const resultText = document.getElementById("result");
   const tipList = document.getElementById("tips");
   const imageTip = document.getElementById("imageTip");
+  const modal = document.getElementById("modal");
 
-  const secret = "turtle";
+  const words = [
+    {
+      name: 'turtle',
+      tip1: 'Podemos encontrá-la tanto na àgua quanto na terra',
+      tip2: 'Ela se movimenta bem devagar'
+    },
+    {
+      name: 'elephant',
+      tip1: 'Ele é muito grande e tem ótima memória',
+      tip2: 'Tem uma tromba muito grande'
+    },
+  ];
+
+  let secret = "";
+  let tip1 = "";
+  let tip2 = "";
 
   let lettersToFind;
   let correctGuess;
   let incorrectGuess;
 
   const createLettersToDiscover = () => {
+
     const lettersContainer = document.getElementsByClassName(
       "letters-container"
     )[0];
@@ -22,9 +40,9 @@ document.addEventListener("DOMContentLoaded", () => {
     for (var i = 0; i < secret.length; i++) {
       const div = document.createElement("div");
       div.classList.add("letter");
-      div.classList.add(secret.toUpperCase()[i]);
       const h2 = document.createElement("h2");
       h2.value = " ";
+      h2.dataset.letter = secret.toUpperCase()[i];
       div.appendChild(h2);
       lettersContainer.appendChild(div);
     }
@@ -46,9 +64,9 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const checkIfGuessIsCorrect = (guess) => {
-    const correctLetters = document.getElementsByClassName(`letter ${guess}`);
+    const correctLetters = document.querySelectorAll(`[data-letter=${guess}]`);
     for (let i = 0; i < correctLetters.length; i++) {
-      correctLetters[i].children[0].innerHTML = guess;
+      correctLetters[i].innerHTML = guess;
     }
     return correctLetters.length;
   };
@@ -76,23 +94,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const showTips = () => {
     if (incorrectGuess == MAX_ATTEMPTS - 3) {
-      const tip1 = document.createElement("li");
-      tip1.innerHTML = "Podemos encontrá-la tanto na àgua quanto na terra";
-      tipList.appendChild(tip1);
+      const tip1element = document.createElement("li");
+      tip1element.innerHTML = tip1;
+      tipList.appendChild(tip1element);
       return;
     }
     if (incorrectGuess == MAX_ATTEMPTS - 2) {
-      const tip2 = document.createElement("li");
-      tip2.innerHTML = "Ela se movimenta bem devagar";
-      tipList.appendChild(tip2);
+      const tip2element = document.createElement("li");
+      tip2element.innerHTML = tip2;
+      tipList.appendChild(tip2element);
       return;
     }
     if (incorrectGuess == MAX_ATTEMPTS - 1) {
-      imageTip.src = "images/turtle.png";
+      imageTip.src = `images/${secret}.png`;
       imageTip.style.display = "block";
       return;
     }
     return;
+  };
+
+  const fillTheWordToDiscover = () => {
+    document.querySelectorAll('[data-letter]').forEach(letter => {
+      letter.innerHTML = letter.dataset.letter;
+    });
   };
 
   const checkIfGameFinished = () => {
@@ -100,12 +124,24 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    resultText.innerHTML =
-      correctGuess == lettersToFind
-        ? "Yeah!!! Congratulations!!!"
-        : "Oh no!!! Try again!!!";
+    if (correctGuess == lettersToFind) {
+      imageResult.src = "images/great-job.png";
+      imageResult.attributes.alt = "Great Job!";
+    } else {
+      imageResult.src = "images/never-give-up.png";
+      imageResult.attributes.alt = "Never give up!";
+    }
+    // resultText.innerHTML =
+    //   correctGuess == lettersToFind
+    //     ? "Yeah!!! Congratulations!!!"
+    //     : "Oh no!!! Try again!!!";
 
-    restartContainer.style.display = "flex";
+    if (incorrectGuess == MAX_ATTEMPTS) {
+      fillTheWordToDiscover();
+    }
+
+    modal.style.display = "flex";
+    // restartContainer.style.display = "flex";
     alphabetContainer.style.display = "none";
 
     return;
@@ -117,13 +153,25 @@ document.addEventListener("DOMContentLoaded", () => {
     imageTip.style.display = "none";
   };
 
+  const suffleOptions = () => {
+    words.sort(() => 0.5 - Math.random());
+
+    const choice = words[0];
+
+    secret = choice.name;
+    tip1 = choice.tip1;
+    tip2 = choice.tip2;
+  };
+
   const createGame = () => {
     clearTips();
-    restartContainer.style.display = "none";
+    modal.style.display = "none";
+    // restartContainer.style.display = "none";
     alphabetContainer.style.display = "block";
 
     correctGuess = 0;
     incorrectGuess = 0;
+    suffleOptions();
     lettersToFind = secret.length;
     updateHangman();
     createLettersToDiscover();
